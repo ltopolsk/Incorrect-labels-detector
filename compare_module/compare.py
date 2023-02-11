fff731ec677416ef1ebfad11169d3b724730ab07
Łukasz Topolski <l.topolski@stud.elka.pw.edu.pl>
2023-02-11 09:28:22 +0100

    nms and mean boxes refactor

--- compare_module/compare.py
+++ compare_module/compare.py
@@ -159,7 +159,7 @@ def pair_targets(targets):
     return bboxes_ret, labels_ret
 
 
-def nms(boxes, scores, threshold):
+def nms(boxes, labels, scores, threshold, func=None):
     if len(boxes) == 0:
         return []
 
@@ -171,11 +171,12 @@ def nms(boxes, scores, threshold):
     areas = (xmax - xmin) * (ymax - ymin)
     order = np.argsort(scores)
 
-    keep = []
+    keep_boxes = []
+    keep_labels = []
 
     while len(order) > 0:
         idx = order[-1]
-        keep.append(boxes[idx])
+
         order = order[:-1]
 
         if len(order) == 0:
@@ -191,63 +192,45 @@ def nms(boxes, scores, threshold):
         intersection = w*h
         iou = intersection/(areas[idx]+areas[order]-intersection)
 
+        if func is not None:
+            to_mean = order[np.where(iou >= threshold)]
+            box_keep = func(to_mean, boxes, labels, scores, idx)
+            keep_boxes.append(box_keep)
+        else:
+            keep_boxes.append(boxes[idx])
+        keep_labels.append(labels[idx])
         left = np.where(iou < threshold)
         order = order[left]
-    return keep
+    return keep_boxes, keep_labels
 
 
-def mean_s(boxes, scores, labels, threshold):
-    if len(boxes) == 0:
-        return []
+def mean_bbox(mean_idx, boxes, labels, scores, idx):
 
     xmin = boxes[:, 0]
     ymin = boxes[:, 1]
     xmax = boxes[:, 2]
     ymax = boxes[:, 3]
 
-    areas = (xmax - xmin) * (ymax - ymin)
-    order = np.argsort(scores)
+    weights = np.array([scores[idx]])
+    xmin_m = np.array([xmin[idx]])
+    ymin_m = np.array([ymin[idx]])
+    xmax_m = np.array([xmax[idx]])
+    ymax_m = np.array([ymax[idx]])
 
-    keep = []
+    for _idx in mean_idx:
+        if labels[_idx] == labels[idx]:
+            weights = np.append(weights, scores[_idx])
+            xmin_m = np.append(xmin_m, xmin[_idx])
+            ymin_m = np.append(ymin_m, ymin[_idx])
+            xmax_m = np.append(xmax_m, xmax[_idx])
+            ymax_m = np.append(ymax_m, ymax[_idx])
 
-    while len(order) > 0:
-        idx = order[-1]
-        # keep.append(boxes[idx])
+    mean_xmin = round(np.average(xmin_m, weights=weights), 2)
+    mean_ymin = round(np.average(ymin_m, weights=weights), 2)
+    mean_xmax = round(np.average(xmax_m, weights=weights), 2)
+    mean_ymax = round(np.average(ymax_m, weights=weights), 2)
 
-        order = order[:-1]
-        if len(order) == 0:
-            break
-
-        xxmin = np.maximum(xmin[idx], xmin[order])
-        yymin = np.maximum(ymin[idx], ymin[order])
-        xxmax = np.minimum(xmax[idx], xmax[order])
-        yymax = np.minimum(ymax[idx], ymax[order])
-        w = np.maximum(0.0, xxmax - xxmin + 1)
-        h = np.maximum(0.0, yymax - yymin + 1)
-        intersection = w*h
-        iou = intersection/(areas[idx]+areas[order]-intersection)
-
-        to_mean = order[np.where(iou >= threshold)]
-        weights = np.array([scores[idx]])
-        xmin_m = np.array([xmin[idx]])
-        ymin_m = np.array([ymin[idx]])
-        xmax_m = np.array([xmax[idx]])
-        ymax_m = np.array([ymax[idx]])
-        for _idx in to_mean:
-            if labels[_idx] == labels[idx]:
-                weights = np.append(weights, scores[_idx])
-                xmin_m = np.append(xmin_m, xmin[_idx])
-                ymin_m = np.append(ymin_m, ymin[_idx])
-                xmax_m = np.append(xmax_m, xmax[_idx])
-                ymax_m = np.append(ymax_m, ymax[_idx])
-        mean_xmin = round(np.average(xmin_m, weights=weights), 2)
-        mean_ymin = round(np.average(ymin_m, weights=weights), 2)
-        mean_xmax = round(np.average(xmax_m, weights=weights), 2)
-        mean_ymax = round(np.average(ymax_m, weights=weights), 2)
-        keep.append(np.array([mean_xmin, mean_ymin, mean_xmax, mean_ymax]))
-        left = np.where(iou < threshold)
-        order = order[left]
-    return keep
+    return np.array([mean_xmin, mean_ymin, mean_xmax, mean_ymax])
 
 
 def average_bboxes(bboxes_sets):
--- tests/test_nms.py
+++ tests/test_nms.py
@@ -1,5 +1,5 @@
 import numpy as np
-from compare_module.compare import nms, mean_s
+from compare_module.compare import mean_bbox, nms
 
 
 def test_nms():
@@ -8,7 +8,8 @@ def test_nms():
                       [10.0, 20.0, 40.0, 65.0],
                       [20.0, 5.0, 27.0, 60.0], ])
     scores = np.array([0.9, 0.7, 0.5, 0.1])
-    nms_res = nms(boxes, scores, 0.3)
+    labels = np.array([1, 1, 1, 1])
+    nms_res, _ = nms(boxes, labels, scores, 0.3)
     # print(nms_res)
     test_boxes = [np.array([10.0, 15.0, 25.0, 60.0])]
     assert len(nms_res) == len(test_boxes)
@@ -23,8 +24,8 @@ def test_mean_s():
                       [20.0, 5.0, 27.0, 60.0], ])
     scores = np.array([0.9, 0.7, 0.5, 0.1])
     labels = np.array([1, 1, 1, 1])
-    nms_res = mean_s(boxes, scores, labels, 0.3)
+    mean_res, _ = nms(boxes, labels, scores, 0.3, func=mean_bbox)
     test_boxes = [np.array([10.77, 15.05, 28.5, 59.55])]
-    assert len(nms_res) == len(test_boxes)
-    for nms_box, test_box in zip(nms_res, test_boxes):
-        assert np.array_equal(nms_box, test_box)
+    assert len(mean_res) == len(test_boxes)
+    for mean_box, test_box in zip(mean_res, test_boxes):
+        assert np.array_equal(mean_box, test_box)
