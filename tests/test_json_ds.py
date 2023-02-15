c5993ca6ce2e77866d17fb928cb16f7b74ee313c
Łukasz Topolski <l.topolski@stud.elka.pw.edu.pl>
2023-02-15 11:08:35 +0100

    json dataset err bbox size  tests

--- tests/test_json_ds.py
+++ tests/test_json_ds.py
@@ -22,3 +22,13 @@ def test_json_err_label():
     assert np.array_equal(res['boxes_test'], np.array([[11.0, 7.0, 497.0, 351.0], [239.0, 47.0, 370.0, 194.0]]))
     assert np.array_equal(res['labels_test'], np.array([19, 8]))
     assert np.array_equal(res['errs'], np.array([-1, -1]))
+
+
+def test_json_err_bbox_size():
+
+    res = ds[8]
+    assert np.array_equal(res['boxes_mean'], np.array([[56.57, 180.12, 283.38, 465.90]], dtype=np.float32))
+    assert np.array_equal(res['labels_mean'], np.array([18]))
+    assert np.array_equal(res['boxes_test'], np.array([[76.0, 40.0, 254.0, 429.0]]))
+    assert np.array_equal(res['labels_test'], np.array([18]))
+    assert np.array_equal(res['errs'], np.array([-2]))
